fafd2893774f039dd0e6026846205555e4ac3d92
Pedro Salazar <salazar9_5@hotmail.com>
2026-09-15 12:54:12 -0700

    first commit

---
+++ IntroPOO2026/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+DateTime nacimiento = Convert.ToDateTime("1995/09/25");
+//Instancia de la clase Persona
+Persona persona = new Persona(name: "Juan", apellidoP: "Perez", apellidoM: "Hernandez", fechaNacimiento: nacimiento);
+
+
+DateTime nacimiento2 = Convert.ToDateTime("1995/09/14");
+Persona persona2 = new Persona(apellidoP: "Garcia", apellidoM: "Lopez", fechaNacimiento: nacimiento2, name: "Maria");
+
+
+//Mostrar en pantalla los datos asignados
+Console.WriteLine($"la persona 1 es {persona.Name} {persona.ApellidoP} " +
+    $"{persona.ApellidoM} y tiene la edad de:{persona.Edad} años");
+
+Console.WriteLine($"la persona 2 es {persona2.Name} {persona2.ApellidoP} " +
+    $"{persona2.ApellidoM} y tiene la edad de:{persona2.Edad} años");
---
+++ IntroPOO2026/clases/Persona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+internal class Persona
+{
+
+    public string Name { get; }
+    public int Edad { get; }
+    public string ApellidoP { get; }
+    public string ApellidoM { get; }
+    public DateTime FechaNacimiento { get; }
+
+    public Persona(string name, string apellidoP, string apellidoM, DateTime fechaNacimiento)
+    {
+        Name = name;
+        FechaNacimiento = fechaNacimiento;
+        Edad = CalcularEdad(fechaNacimiento);
+        ApellidoP = apellidoP;
+        ApellidoM = apellidoM;
+    }
+    
+    private int CalcularEdad(DateTime fechaNacimiento)
+    { 
+        DateTime hoy = DateTime.Today;
+
+        int Edad = hoy.Year - fechaNacimiento.Year;
+
+        DateTime aux = hoy.AddYears(-Edad);
+
+        if (fechaNacimiento.Date > aux)
+        {
+            Edad--;
+        }
+        return Edad;
+    }
+}
+
